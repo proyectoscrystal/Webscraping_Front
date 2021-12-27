2940915545b8223e6a8856c1f2a775809af544b7
juanDev-47 <juan1037av@gmail.com>
2021-12-27 14:33:42 -0500

    se configura ambiente de produccion

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,24 +1,8 @@
 export class AppSettings {
   // Variables Producción
-  // public static Captcha: string = '6LecJxkaAAAAAHvRKyd-Q2ynk_YWtjIvyhHt04V8';
-  // public static Api: string = 'https://blackboxapp01.azurewebsites.net';
-  // public static Model: string ='https://blackboxapp01.azurewebsites.net/sendImgsModel';
-  // public static Filter: string = 'https://blackboxapp01.azurewebsites.net/filtroImagen';
+  public static Captcha: string = '6LecJxkaAAAAAHvRKyd-Q2ynk_YWtjIvyhHt04V8';
+  public static Api: string = 'https://blackboxapp01.azurewebsites.net';
+  public static Model: string ='https://blackboxapp01.azurewebsites.net/sendImgsModel';
+  public static Filter: string = 'https://blackboxapp01.azurewebsites.net/filtroImagen';
 
-  // Variables desarrollo local
-  public static Captcha: string = '6LcWmhAaAAAAAANjKl0-wVi86F_MCNqgZ490x7Sc';
-  public static Api: string = 'http://localhost:4000';
-  public static Model: string = 'http://localhost:4000/sendImgsModel';
-  public static Filter: string = 'http://localhost:4000/filtroImagen';
-
-  // Variable de prueba heroku
-    // public static Captcha :string = '6LfSkxAaAAAAAJjbsB9OzDdWDC-5KGdgHjHqsJr_';
-    // public static Api: string = 'https://backend-scraping-1.herokuapp.com';
-    // public static Model: string = 'https://backend-scraping-1.herokuapp.com/sendImgsModel';
-    // public static Filter: string = 'https://backend-scraping-1.herokuapp.com/filtroImagen';
-
-  // public static Captcha :string = '6LfSkxAaAAAAAJjbsB9OzDdWDC-5KGdgHjHqsJr_';
-  // public static Api: string = 'https://scrapingblackbox.herokuapp.com';
-  // public static Model: string = 'https://scrapingblackbox.herokuapp.com/sendImgsModel';
-  // public static Filter: string = 'https://scrapingblackbox.herokuapp.com/filtroImagen';
 }
